2fe858769c5239b18dac2165469c164225cc7339
Daniel Post <daniel@danielpost.com>
2025-01-18 18:15:48 -0500

    Cleanup custom table after test (#17)

--- tests/_support/Helper/Wpunit.php
+++ tests/_support/Helper/Wpunit.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Helper;
 
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
-
-class Wpunit extends \Codeception\Module
-{
-
+class Wpunit extends \Codeception\Module {
+	public function _after(\Codeception\TestInterface $test) {
+		// Remove all logs after each test.
+		global $wpdb;
+		$wpdb->query(
+			$wpdb->prepare(
+				'DELETE FROM %i',
+				$wpdb->prefix . \Autoblue\Logging\DatabaseHandler::TABLE_NAME
+			)
+    );
+	}
 }
